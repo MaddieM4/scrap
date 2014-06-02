52b275c08fdeebabea16df5bcb6e2afbb9c2ccd1
Philip Horger <campadrenalin@gmail.com>
2014-06-01 22:07:55 -0700

    Tests and better organization for request.go

--- request.go
+++ request.go
@@ -15,21 +15,38 @@ type ScraperRequest struct {
 	Debug        *log.Logger
 }
 
-// Queue another URL for scraping. Duplicate queued items are ignored.
-func (sr ScraperRequest) QueueAnother(queue_url string) {
+// De-relativize a URL based on the existing request's URL.
+//
+// This is how "/foo/" URLs queued up for scraping are turned into
+// more actionable "http://origin.host.name/foo/" URLs.
+//
+// Current behavior is to only give an absolute URL out, if the
+// request's URL is absolute. The contextualization is only as
+// good as the request URL's "absoluteness". You won't get an error
+// if the result is ambiguous. THIS MAY CHANGE IN FUTURE RELEASES.
+func (sr ScraperRequest) ContextualizeUrl(rel_url string) (string, error) {
 	old_url, err := url.Parse(sr.Url)
 	if err != nil {
-		sr.Debug.Println(err.Error())
-		return
+		return "", err
 	}
-	new_url, err := url.Parse(queue_url)
+
+	new_url, err := url.Parse(rel_url)
 	if err != nil {
-		sr.Debug.Println(err.Error())
-		return
+		return "", err
 	}
+
 	new_url.Fragment = "" // Treat all #whatever urls the same
 	final_url := old_url.ResolveReference(new_url)
+	return final_url.String(), nil
+}
 
-	new_req := sr.RequestQueue.CreateRequest(final_url.String())
+// Queue another URL for scraping. Duplicate queued items are ignored.
+func (sr ScraperRequest) QueueAnother(queue_url string) {
+	abs_url, err := sr.ContextualizeUrl(queue_url)
+	if err != nil {
+		sr.Debug.Println(err)
+		return
+	}
+	new_req := sr.RequestQueue.CreateRequest(abs_url)
 	sr.RequestQueue.DoRequest(new_req)
 }
--- request_test.go
+++ request_test.go
@@ -1,3 +1,117 @@
 package scrap
 
-// TODO: Repopulate
+import "testing"
+
+type ContextualizeUrlTest struct {
+	SR     string
+	Input  string
+	Output string
+	Error  bool
+}
+
+func TestSR_ContextualizeUrl(t *testing.T) {
+	tests := []ContextualizeUrlTest{
+		ContextualizeUrlTest{
+			"/foo/",
+			"/bar/",
+			"/bar/",
+			false,
+		},
+		ContextualizeUrlTest{
+			"http://hostname/foo/",
+			"/bar/",
+			"http://hostname/bar/",
+			false,
+		},
+		ContextualizeUrlTest{
+			"http://hostname/foo/",
+			"https://other.hostname/bar/",
+			"https://other.hostname/bar/",
+			false,
+		},
+		ContextualizeUrlTest{
+			"",
+			"/bar/",
+			"/bar/",
+			false,
+		},
+		ContextualizeUrlTest{
+			"",
+			"https://other.hostname/bar/",
+			"https://other.hostname/bar/",
+			false,
+		},
+		ContextualizeUrlTest{
+			"https://hostname/foo/",
+			"",
+			"https://hostname/foo/",
+			false,
+		},
+		ContextualizeUrlTest{
+			"/foo/",
+			"",
+			"/foo/",
+			false,
+		},
+		ContextualizeUrlTest{
+			"",
+			"",
+			"",
+			false,
+		},
+		ContextualizeUrlTest{
+			"%",
+			"/bar/",
+			"parse %: invalid URL escape \"%\"",
+			true,
+		},
+		ContextualizeUrlTest{
+			"/foo/",
+			"%",
+			"parse %: invalid URL escape \"%\"",
+			true,
+		},
+	}
+	for _, test := range tests {
+		var req ScraperRequest
+		req.Url = test.SR
+
+		got, err := req.ContextualizeUrl(test.Input)
+		if test.Error && err == nil {
+			t.Fatal("Should have failed: %#v", test.Input)
+		} else if !test.Error && err != nil {
+			t.Fatal(err)
+		}
+
+		if test.Error {
+			got = err.Error()
+		}
+		compare(t, test.Output, got)
+	}
+}
+
+func TestSR_QueueAnother(t *testing.T) {
+	trq := NewTestRQ()
+	req := trq.CreateRequest("http://hostname/pasta")
+	req.QueueAnother("/sauce")
+
+	if len(trq.Queued) != 1 {
+		t.Fatalf("Should have queued one item - %d queued", len(trq.Queued))
+	}
+	compare(t, "http://hostname/sauce", trq.Queued[0].Url)
+	compare(t, "", trq.Debug.String())
+}
+
+func TestSR_QueueAnother_BadUrl(t *testing.T) {
+	trq := NewTestRQ()
+	req := trq.CreateRequest("http://hostname/pasta")
+	req.QueueAnother("%")
+
+	if len(trq.Queued) != 0 {
+		t.Fatalf("Should not have queued any items - %d queued", len(trq.Queued))
+	}
+	compare(t,
+		"http://hostname/pasta: parse %: invalid URL escape \"%\"\n",
+		trq.Debug.String(),
+	)
+}
