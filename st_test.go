31967c34a5e088b52484bae8dc525ba2b61fc9c0
Philip Horger <campadrenalin@gmail.com>
2014-06-02 20:56:46 -0700

    Use st instead of selector

--- st_test.go
+++ st_test.go
@@ -3,54 +3,54 @@ package scrap
 import "testing"
 
 type generator func(string) StringTest
-type selector_test struct {
+type st_test struct {
 	Pattern string
 	Url     string
 	Match   bool
 }
 
-func (st selector_test) Run(t *testing.T, gen generator) {
-	selector := gen(st.Pattern)
-	got := selector(st.Url)
-	compare(t, st.Match, got)
+func (stt st_test) Run(t *testing.T, gen generator) {
+	st := gen(stt.Pattern)
+	got := st(stt.Url)
+	compare(t, stt.Match, got)
 }
 
-type selector_test_slice []selector_test
+type st_test_slice []st_test
 
-func (st_slice selector_test_slice) Run(t *testing.T, gen generator) {
-	for _, st := range st_slice {
-		st.Run(t, gen)
+func (st_slice st_test_slice) Run(t *testing.T, gen generator) {
+	for _, stt := range st_slice {
+		stt.Run(t, gen)
 	}
 }
 
 func TestSTExact(t *testing.T) {
-	selector_test_slice{
-		selector_test{
+	st_test_slice{
+		st_test{
 			Pattern: "a pattern for exact matches",
 			Url:     "a pattern for exact matches",
 			Match:   true,
 		},
-		selector_test{
+		st_test{
 			Pattern: "a pattern for exact matches",
 			Url:     "a pattern",
 			Match:   false,
 		},
-		selector_test{
+		st_test{
 			Pattern: "a pattern for exact matches",
 			Url:     "exact matches",
 			Match:   false,
 		},
-		selector_test{
+		st_test{
 			Pattern: "a pattern for exact matches",
 			Url:     "a pattern for exact matches, with some at the end",
 			Match:   false,
 		},
-		selector_test{
+		st_test{
 			Pattern: "a pattern for exact matches",
 			Url:     "Before a pattern for exact matches",
 			Match:   false,
 		},
-		selector_test{
+		st_test{
 			Pattern: "a pattern for exact matches",
 			Url:     "A Pattern For Exact Matches",
 			Match:   false,
@@ -59,33 +59,33 @@ func TestSTExact(t *testing.T) {
 }
 
 func TestSTPrefix(t *testing.T) {
-	selector_test_slice{
-		selector_test{
+	st_test_slice{
+		st_test{
 			Pattern: "a pattern for exact matches",
 			Url:     "a pattern for exact matches",
 			Match:   true,
 		},
-		selector_test{
+		st_test{
 			Pattern: "a pattern for exact matches",
 			Url:     "a pattern",
 			Match:   false,
 		},
-		selector_test{
+		st_test{
 			Pattern: "a pattern for exact matches",
 			Url:     "exact matches",
 			Match:   false,
 		},
-		selector_test{
+		st_test{
 			Pattern: "a pattern for exact matches",
 			Url:     "a pattern for exact matches, with some at the end",
 			Match:   true,
 		},
-		selector_test{
+		st_test{
 			Pattern: "a pattern for exact matches",
 			Url:     "Before a pattern for exact matches",
 			Match:   false,
 		},
-		selector_test{
+		st_test{
 			Pattern: "a pattern for exact matches",
 			Url:     "A Pattern For Exact Matches",
 			Match:   false,
